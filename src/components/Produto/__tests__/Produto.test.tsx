621812b5bb0e4b3c92baad1cf941906e09b4a07f
Henrique Sanfilippo <henrique.sanfilippo@gmail.com>
2025-05-13 22:48:07 -0300

    feat: test unitario de render e texto em Produto

---
+++ src/components/Produto/__tests__/Produto.test.tsx
@@ -0,0 +1,20 @@
+import { screen } from '@testing-library/react'
+
+import Produto from '..'
+import { renderizaComProvider } from '../../../utils/tests'
+
+describe('Testes para o componente produto', () => {
+  test('Deve renderizar corretamente', () => {
+    const jogo = {
+      id: 2,
+      categoria: 'RPG',
+      imagem: '',
+      plataformas: ['Windows', 'PS5', 'Xbox Series S/X'],
+      preco: 199.9,
+      precoAntigo: 299.9,
+      titulo: 'Hogwarts Legacy'
+    }
+    renderizaComProvider(<Produto game={jogo} />)
+    expect(screen.getByText('Hogwarts Legacy')).toBeInTheDocument()
+  })
+})
